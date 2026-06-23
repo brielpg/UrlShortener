50012e5b5656a8df736a4a1b1ff7bf54b402e79d
Gabriel Galiza <briel.galiza@gmail.com>
2026-06-23 17:56:15 -0300

    feat: create url service and add method findMaxSequentialId in the url repository

---
+++ url-shortener/src/main/java/br/com/url_shortener/application/services/UrlService.java
@@ -0,0 +1,51 @@
+package br.com.url_shortener.application.services;
+
+import br.com.url_shortener.domain.models.Url;
+import br.com.url_shortener.infrastructure.repositories.UrlRepository;
+import jakarta.annotation.PostConstruct;
+import org.hashids.Hashids;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UrlService {
+    private static final String COUNTER_KEY = "url:counter";
+
+    private final UrlRepository repository;
+    private final StringRedisTemplate redisTemplate;
+    private final Hashids hashids;
+
+    public UrlService(UrlRepository repository, StringRedisTemplate redisTemplate, Hashids hashids) {
+        this.repository = repository;
+        this.redisTemplate = redisTemplate;
+        this.hashids = hashids;
+    }
+
+    @PostConstruct
+    public void initCounter() {
+        Long maxId = repository.findMaxSequentialId();
+        redisTemplate.opsForValue().setIfAbsent(COUNTER_KEY, String.valueOf(maxId));
+    }
+
+    @Transactional
+    public String createShortUrl(String originalUrl) {
+        if (originalUrl == null || originalUrl.isBlank())
+            throw new RuntimeException("URL is required");
+
+        Long id = redisTemplate.opsForValue().increment(COUNTER_KEY);
+        String shortCode = hashids.encode(id);
+
+        Url shortUrl = new Url(shortCode, originalUrl, id);
+        repository.save(shortUrl);
+
+        return shortCode;
+    }
+
+    @Transactional(readOnly = true)
+    public String getOriginalUrl(String shorterCode) {
+        return repository.findById(shorterCode)
+                .orElseThrow(() -> new RuntimeException("URL not found"))
+                .getOriginalUrl();
+    }
+}
--- url-shortener/src/main/java/br/com/url_shortener/infrastructure/repositories/UrlRepository.java
+++ url-shortener/src/main/java/br/com/url_shortener/infrastructure/repositories/UrlRepository.java
@@ -2,8 +2,12 @@ package br.com.url_shortener.infrastructure.repositories;
 
 import br.com.url_shortener.domain.models.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, String> {
+
+    @Query("SELECT COALESCE(MAX(u.sequentialId), 0) FROM URL u")
+    Long findMaxSequentialId();
 }
