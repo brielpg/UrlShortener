d0bf69091766608fb7e0bea7268001031792e729
Gabriel Galiza <briel.galiza@gmail.com>
2026-06-23 20:11:36 -0300

    feat: add url validation in the url request dto

--- url-shortener/src/main/java/br/com/url_shortener/application/dtos/UrlRequest.java
+++ url-shortener/src/main/java/br/com/url_shortener/application/dtos/UrlRequest.java
@@ -1,9 +1,11 @@
 package br.com.url_shortener.application.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 public record UrlRequest(
         @NotBlank(message = "URL is required")
+        @URL(message = "Invalid URL format")
         String url
 ) {
 }
